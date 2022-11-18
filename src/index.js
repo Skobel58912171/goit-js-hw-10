7985863e7d3a455835a5602a2326317507253e25
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-11-18 07:05:41 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const searchInput = document.querySelector('#search-box');
+console.log(searchInput);
+// function fetchCountries(name) {
+//   const name = 'Canada';
+//   const options = {
+//     headers: {
+//       authorization: '',
+//     },
+//   };
+// const url = https://restcountries.com/v3.1/name/{name};
+// fetch(url)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(name => console.log(name))
+//   .catch(error => {
+//     console.log(error);
+//   });
+// }
+fetch('https://restcountries.com/v3.1/name/peru')
+  .then(response => {
+    return response.json();
+  })
+  .then(name => console.log(name))
+  .catch(error => {
+    console.log(error);
+  });
