a0720306f7eaef094b8bd316200fc47836c9675b
Skobel58912171 <106476498+Skobel58912171@users.noreply.github.com>
2022-11-19 19:40:17 +0000

    Deploying to gh-pages from @ Skobel58912171/goit-js-hw-10@6fdf0634ad0c4e7d78b5581b9427cac6baf0027d 🚀

--- index.9fead5b1.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("#search-box");console.log(o),fetch("https://restcountries.com/v3.1/name/peru").then((o=>o.json())).then((o=>console.log(o))).catch((o=>{console.log(o)}));
-//# sourceMappingURL=index.9fead5b1.js.map
